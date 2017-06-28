c9d9d71053aedce181ecc8816c4929960a9f1d48
李伟 <mrgoon@foxmail.com>
2017-06-28 19:20:52 +0800

    update

--- src/AliSms.php
+++ src/AliSms.php
@@ -3,11 +3,15 @@
 
 namespace Mrgoon\AliSms;
 
-use Mrgoon\ApiSdk\Dysmsapi\Request\V20170525\SendSmsRequest;
+use Mrgoon\AliyunSmsSdk\DefaultAcsClient;
+use Mrgoon\AliyunSmsSdk\Profile\DefaultProfile;
+use Mrgoon\Dysmsapi\Request\V20170525\SendSmsRequest;
 
 class AliSms {
-    public function sendSms($to, $template_code, $data)
+    public function sendSms($to, $template_code, $data, $outId = '')
     {
+
+
         //此处需要替换成自己的AK信息
         $accessKeyId = config('aliyunsms.access_key');
         $accessKeySecret = config('aliyunsms.access_secret');
@@ -22,9 +26,10 @@ class AliSms {
         $region = "cn-hangzhou";
 
         //初始化访问的acsCleint
-        $profile = \DefaultProfile::getProfile($region, $accessKeyId, $accessKeySecret);
-        \DefaultProfile::addEndpoint("cn-hangzhou", "cn-hangzhou", $product, $domain);
-        $acsClient= new \DefaultAcsClient($profile);
+
+        $profile = DefaultProfile::getProfile($region, $accessKeyId, $accessKeySecret);
+        DefaultProfile::addEndpoint("cn-hangzhou", "cn-hangzhou", $product, $domain);
+        $acsClient= new DefaultAcsClient($profile);
 
         $request = new SendSmsRequest();
         //必填-短信接收号码
@@ -39,11 +44,12 @@ class AliSms {
         }
 
         //选填-发送短信流水号
-        $request->setOutId("1234");
+        if ($outId) {
+            $request->setOutId($outId);
+        }
 
         //发起访问请求
-        $acsResponse = $acsClient->getAcsResponse($request);
-        var_dump($acsResponse);
+        return $acsClient->getAcsResponse($request);
     }
 }
 
